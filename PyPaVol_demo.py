9a81e6000900bfef98b3a4a89e2b3de53d8acebb
Daniel Callejas Sevilla <daniel.callejas.sevilla@gmail.com>
2017-05-19 18:40:26 +0200

    Get tracks and sinks from pulsectl

--- PyPaVol_demo.py
+++ PyPaVol_demo.py
@@ -4,7 +4,26 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QSlider, QCheckBox, QApplication
 from PyQt5.QtCore import Qt
-import random
+
+import pulsectl
+
+pulse = pulsectl.Pulse('PyPaVol') # TODO get rid of global object
+
+# temp method for the demo; final solution must populate from pulseaudio events
+# TODO can a track go to multiple sinks? if so, format of returned tracks needs to evolve
+def get_tracks():
+    tracks = []
+    for t in pulse.sink_input_list():
+        track = (t.proplist['application.name'], t.sink, 100 * t.volume.value_flat)
+        tracks.append(track)
+    return tracks
+
+def get_sinks():
+    sinks = []
+    for s in pulse.sink_list():
+        sink = (s.description, 100 * s.volume.value_flat)
+        sinks.append(sink)
+    return sinks
 
 class PyPaVol_demo(QWidget):
 
@@ -13,37 +32,33 @@ class PyPaVol_demo(QWidget):
         self.initUI()
         
     def initUI(self):
+        
         grid = QGridLayout()
         self.setLayout(grid)
         
-        tracks = [ ["Amarok",     [0, 1, 0, 0], 100],
-                   ["Chrome",     [0, 0, 1, 0], 100],
-                   ["Half-Life",  [0, 0, 0, 1], 100],
-                   ["VLC",        [0, 0, 1, 0], 100],
-                   ["USB Webcam", [0, 0, 0, 0], 100],
-                   ["M2496 Line", [1, 0, 0, 0], 100],
-                   ["Intel Line", [0, 0, 0, 0], 100]
-                 ]
-        sinks = [ ["Intel",    30],
-                     ["M2496",    50],
-                     ["La Tele", 100],
-                     ["Ardour",  100]
-                   ]
+        tracks = get_tracks()
+        sinks = get_sinks()
         
         for i, track in enumerate(tracks):
             print(i, track)
-            name, patches, vol = track
+            name, sink, vol = track
             l = QLabel(name)
             l.setMaximumWidth(80)
             l.setMinimumWidth(80)
             grid.addWidget(l, 0, i)
-            for j, p in enumerate(patches):
+            for j, _ in enumerate(sinks):
                 c = QCheckBox(self)
-                c.setChecked(p == 1)
+                c.setChecked(j == sink)
                 grid.addWidget(c, j + 1, i)
             s = QSlider(Qt.Vertical, self)
+            s.setMaximum(153)
+            s.setMinimum(0)
             s.setValue(vol)
-            grid.addWidget(s, len(patches) + 2, i)
+            grid.addWidget(s, len(sinks) + 2, i)
+            l = QLabel(str(vol))
+            l.setMaximumWidth(80)
+            l.setMinimumWidth(80)
+            grid.addWidget(l, len(sinks) + 3, i)
             
         skip_h = len(tracks) + 1
         skip_v = len(sinks) + 1
@@ -53,6 +68,8 @@ class PyPaVol_demo(QWidget):
             l = QLabel(name)
             grid.addWidget(l, skip_v, skip_h + i)
             s = QSlider(Qt.Vertical, self)
+            s.setMaximum(153)
+            s.setMinimum(0)
             s.setValue(vol)
             grid.addWidget(s, skip_v + 1, skip_h + i)
                     
